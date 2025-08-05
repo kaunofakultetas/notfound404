66ce3f4b82d82f042e4e56b5e28f3dca18cb4b7e
Tomas Vanagas <tomas.vanagas@knf.vu.lt>
2025-08-05 14:09:47 +0300

    Commit all

---
+++ nextjs/src/components/Head/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export const HeadComponent = () => (
+  <Head>
+    <title>KNF - Paklydote</title>
+    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    <meta
+      name="description"
+      content=""
+    />
+    <meta
+      name="keywords"
+      content=""
+    />
+  </Head>
+)
